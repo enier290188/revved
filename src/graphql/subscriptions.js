b02e764502ba032675e108da6af75c9d90a0f0c0
Enier Ramos García <enier290188@gmail.com>
2022-12-09 07:56:26 -0600

    Updated Amplify-Api.

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,34 +1,46 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateTodo = /* GraphQL */ `
-  subscription OnCreateTodo {
-    onCreateTodo {
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
       id
+      picture
+      email
       name
-      description
+      userGroupList
+      cognitoUsername
+      cognitoUserStatus
       createdAt
       updatedAt
     }
   }
 `;
-export const onUpdateTodo = /* GraphQL */ `
-  subscription OnUpdateTodo {
-    onUpdateTodo {
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
       id
+      picture
+      email
       name
-      description
+      userGroupList
+      cognitoUsername
+      cognitoUserStatus
       createdAt
       updatedAt
     }
   }
 `;
-export const onDeleteTodo = /* GraphQL */ `
-  subscription OnDeleteTodo {
-    onDeleteTodo {
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
       id
+      picture
+      email
       name
-      description
+      userGroupList
+      cognitoUsername
+      cognitoUserStatus
       createdAt
       updatedAt
     }

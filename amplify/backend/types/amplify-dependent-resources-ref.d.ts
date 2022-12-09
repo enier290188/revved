c661b2283145cba46ba48ae5f4c3a228d026ee42
Enier Ramos García <enier290188@gmail.com>
2022-12-08 18:54:00 -0600

    Adding Amplify-Storage.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -18,5 +18,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "revved": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
